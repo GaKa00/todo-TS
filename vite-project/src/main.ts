cdb33e34bd4ee5a4709218a2fed8d60fa0a8c62f
Gabriel Kassarp <gkassarp@gmail.com>
2024-02-08 15:16:54 +0100

    todo should be done

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,24 +1,82 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+type Task = {
+  id: string;
+  description: string
+  status:string
+  completed: boolean
+}
+  let i = 0;
+  let item: Task;
+ 
+const table = document.querySelector('#table') as HTMLTableElement
+const taskQuery = document.querySelector('.query') as HTMLInputElement
+const newTaskBtn = document.querySelector('.create') as HTMLButtonElement
+
+function taskCounter () {
+  i++
+
+}
+
+
+newTaskBtn.addEventListener('click', ()=>{
+  
+  taskCounter()
+item.id =i.toString();
+item.status = 'In Progress';
+item.description = taskQuery.value;
+item.completed = false;
+console.log(item)
+appendTask(item);
+
+
+})
+
+  
+
+
+
+// create a function that counts tasks and sets count to task.id asfter parsing it to string -- Done, almost
+
+//create a eventListener that takes the written query and sets it to task.description -- Should be done
+//set default value of task.status to 'In progress' and task.completed to false -- done
+//make finished button set task.status to completed  -- not done
+// make delete buttons work -- not done
+
+
+
+function appendTask(item:Task){
+  const newRow = document.createElement('tr');
+
+ newRow.innerHTML = `
+            <th scope="row">${item.id}</th>
+            <td contenteditable="true">${item.description}</td>
+            <td contenteditable="true">${item.status}</td>
+            <td>
+                <button type="submit" class="btn btn-danger">Delete</button>
+                <button type="submit" class="btn btn-success ms-1">Finished</button>
+            </td>
+        `;
+
+      
+        table.getElementsByTagName('tbody')[0].appendChild(newRow);
+
+
+ 
+//   const todoID = document.createElement('th');
+//   const todoDesc = document.createElement('td');
+//   const todoStatus = document.createElement('td');
+// ;
+
+//   todoID.textContent = item.id; //add scope row to id
+//   todoDesc.textContent = item.description; //add contenteditable
+//   todoStatus.textContent = item.status;  //add contenteditable
+
+// newRow.appendChild(todoID);
+// newRow.appendChild(todoDesc);
+// newRow.appendChild(todoStatus);
+
+
+
+//   table.appendChild(newRow);
+}
